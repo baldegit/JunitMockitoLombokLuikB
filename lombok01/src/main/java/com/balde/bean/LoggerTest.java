b80a8238d47546e4830cca43162c5f7d7ca76389
Balde Ibrahima <elhibrbalde@gmail.com>
2018-12-05 02:03:43 -0800

    ce projet contient les annotations de base de lombok

---
+++ lombok01/src/main/java/com/balde/bean/LoggerTest.java
@@ -0,0 +1,22 @@
+package com.balde.bean;
+
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
+
+//::ajouter la dependance slf4j dans maven pour utiliser cette @
+//@Slf4j
+//@Log4j
+public class LoggerTest {
+	
+	private String pays;
+	private int age;
+	public LoggerTest() {
+		super();
+		// TODO Auto-generated constructor stub
+//		log.debug("je debug mon code");
+//		log.info("ceci est une info");
+	}
+	
+	
+	
+}
---
+++ lombok01/src/main/java/com/balde/bean/User.java
@@ -0,0 +1,17 @@
+package com.balde.bean;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+public class User {
+	
+	private int id;
+	private String firstName;
+	private String lastName;
+	private int age;
+	
+	
+	
+	
+}
